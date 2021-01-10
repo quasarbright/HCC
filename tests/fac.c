626f1402671545ec4f0072bac67e05a937187b06
Mike Delmonaco <mthundershock@gmail.com>
2021-01-10 16:34:53 -0500

    update test files to use == and ! in conditions

--- tests/fac.c
+++ tests/fac.c
@@ -1,6 +1,6 @@
 int n = 6;
 int fac = 1;
-for(int i = 1; n + -i; i = i + 1;) {
+for(int i = 1; !(n == i); i = i + 1;) {
     fac = fac * i;
 }
 return fac;
--- tests/fib.c
+++ tests/fib.c
@@ -3,7 +3,7 @@ int i = 2;
 int nprev = 1;
 int nprevprev = 1;
 // without comparison, this only works for n >= 2
-while(n + -i) { // TODO integer comparison
+while(!(n == i)) { // TODO integer comparison
     int tmp = nprev + nprevprev;
     nprevprev = nprev;
     nprev = tmp;
