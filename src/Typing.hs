1268b9b84fb4d4f1a3622f50e2ce7fdcf4563134
Mike Delmonaco <mthundershock@gmail.com>
2021-01-11 16:47:08 -0500

    static checking for functions

--- src/Typing.hs
+++ src/Typing.hs
@@ -22,6 +22,8 @@ data TypeError = Mismatch Type Type
                | InternalError String
                | BadVoid Expr
                | BadArrayAccess Type
+               | AppliedNonFunction Type
+               | ArityError Int Int
                deriving(Eq)
 
 instance Show TypeError where
@@ -32,6 +34,8 @@ instance Show TypeError where
         InternalError s -> "Internal type checking error: "++s
         BadVoid e -> "Value cannot be void: "++show e
         BadArrayAccess t -> "Cannot do array access on a value of type "++show t
+        AppliedNonFunction t -> "Called a non-function: "++show t
+        ArityError expected actual -> "Arity error: expected "++show expected++" arguments, but got "++show actual
 
 newtype Checker a = Checker { runChecker :: ExceptT TypeError (Reader Env) a }
                  deriving( Functor
@@ -42,6 +46,9 @@ newtype Checker a = Checker { runChecker :: ExceptT TypeError (Reader Env) a }
                          )
 
 -- utility --
+
+nothing :: Monad m => m ()
+nothing = return ()
 
 lookupVar :: String -> Checker Type
 lookupVar x = do
@@ -82,9 +89,9 @@ inferExpr =
                 TInt -> throwError (BadDeref t)
                 TVoid -> throwError (BadDeref t)
                 TArray{} -> throwError (BadDeref t) -- TODO treat as t*?
+                TFun{} -> throwError (BadDeref t)
         EUnop AddrOf e -> maybe err (const (TRef <$> inferExpr e)) (lhsOfExpr e)
             where err = throwError (InternalError "bad addrOf at typecheck")
-            -- TODO test more relaxed addrOf
         EUnop Neg e -> unop e
         EUnop Not e -> unop e
         EUnop Inv e -> unop e
@@ -106,66 +113,85 @@ inferExpr =
                 TRef{} -> err
                 TInt{} -> err
                 TVoid{} -> err
+                TFun{} -> err
         EArrayLiteral [] -> throwError (InternalError "cannot infer type of empty array literal")
         EArrayLiteral (e:es) -> do
             t <- inferExpr e
             checkExpr t (EArrayLiteral es)
-            return (TArray t Nothing)          
+            return (TArray t Nothing)
+        EApp f args -> do
+            tF <- inferExpr f
+            case tF of
+                TFun ret argts -> do
+                    let nExpected = length argts
+                    let nActual = length args
+                    unless (nExpected == nActual) (throwError (ArityError nExpected nActual))
+                    zipWithM_ checkExpr argts args
+                    return ret
+                _ -> throwError (AppliedNonFunction tF)   
 
 inferLHS :: LHS -> Checker Type
 inferLHS = inferExpr . exprOfLhs
 
-checkBlock :: Type -> [Statement] -> Checker ()
-checkBlock t b = assertEqual t =<< inferBlock b
+-- | ensure a statement is either an effect or returns the given type
+checkStatement :: Type -> Statement -> Checker ()
+checkStatement tExpected stmt = case stmt of
+     Assign lhs rhs -> do
+         t <- inferLHS lhs
+         checkExpr t rhs
+     Decl{} -> nothing
+     Def t _ rhs -> checkExpr t rhs
+     Return e -> checkExpr tExpected e
+     If cnd thn mEls -> do
+         checkExpr TInt cnd
+         checkBlock tExpected thn
+         maybe nothing (checkBlock tExpected) mEls
+     While cnd body -> do
+         checkExpr TInt cnd
+         checkBlock tExpected body
+     For setup cnd update body -> checkBlock tExpected (whileOfFor setup cnd update body)
+    
 
-inferBlock :: [Statement] -> Checker Type
-inferBlock = foldr go (return TVoid)
+checkBlock :: Type ->  [Statement] -> Checker ()
+checkBlock tExpected = go
     where
-        go stmt mRest =
-            case stmt of
-                -- TODO for if else if() t else void is ok!!!
-                Assign lhs rhs -> do
-                    t <- inferLHS lhs
-                    checkExpr t rhs
-                    mRest
+        go [] = nothing
+        go (stmt:rest) =
+            let mRest = go rest
+                mStmt = checkStatement tExpected stmt
+            in mStmt >> case stmt of
+                Assign{} -> mRest
+                Return{} -> mRest
                 Decl t x -> annot x t mRest
-                Def t x rhs -> checkExpr t rhs >> annot x t mRest
-                Return e -> inferExpr e <* mRest -- rest should be empty                
-                If cnd thn Nothing -> do
-                    checkExpr TInt cnd
-                    tThn <- inferBlock thn
-                    case tThn of
-                        TVoid -> mRest
-                        -- TODO use paramorphism and checkBlock
-                        _ -> (assertEqual tThn =<< mRest) >> return tThn
-                If cnd thn (Just els) -> do
-                    checkExpr TInt cnd
-                    tThn <- inferBlock thn
-                    tEls <- inferBlock els
-                    case (tThn, tEls) of
-                        (TVoid,TVoid) -> mRest
-                        (t, TVoid) -> (assertEqual t =<< mRest) >> return t
-                        (TVoid, t) -> (assertEqual t =<< mRest) >> return t
-                        _ -> do
-                            assertEqual tThn tEls
-                            -- TODO use paramorphism and checkBlock
-                            assertEqual tThn =<< mRest
-                            return tThn
-                While cnd body -> do
-                    checkExpr TInt cnd
-                    tBody <- inferBlock body
-                    assertEqual tBody =<< mRest -- TODO change after paramorphism
-                    return tBody
-                -- TODO change after paramorphism
-                For setup cnd update body -> inferBlock (whileOfFor setup cnd update body) >> mRest
-                    
-
-            
-checkProgram :: Type -> Program -> Checker ()
-checkProgram t (Program b) = checkBlock t b
+                Def t x _ -> annot x t mRest
+                If{} -> mRest
+                While{} -> mRest
+                For{} -> mRest
+                
+checkDecl :: TopDecl -> Checker ()
+checkDecl = \case
+    FunDecl{} -> nothing
+    FunDef ret f targs body -> do
+        let tF = TFun ret (fmap fst targs)
+        -- f last bc args have scope precedence: void f(int x, int f) {body}, f :: int in body
+        let annotations = [(x,t) | (t,x) <- targs++[(tF,f)]]
+        annots annotations (checkBlock ret body)
         
-inferProgram :: Program -> Checker Type
-inferProgram (Program b) = inferBlock b
+
+checkDecls :: [TopDecl] -> Checker ()
+checkDecls [] = nothing
+checkDecls (decl:rest) =
+    let mRest = checkDecls rest
+        mDecl = checkDecl decl
+    in mDecl >> case decl of
+        FunDecl ret f _ -> annot f ret mRest
+        FunDef ret f _ _ -> annot f ret mRest
+            
+checkProgram :: Program -> Checker ()
+checkProgram (Program decls) = checkDecls decls
+        
+
+-- monad running --
 
 executeChecker :: Env -> Checker a -> Either TypeError a
 executeChecker env =
@@ -174,7 +200,7 @@ executeChecker env =
     >>> flip runReader env
 
 typeCheckProgram :: Program -> Either TypeError ()
-typeCheckProgram = executeChecker Map.empty . void . inferProgram
+typeCheckProgram = executeChecker Map.empty . void . checkProgram
 
 
     
--- src/WellFormedness.hs
+++ src/WellFormedness.hs
@@ -3,6 +3,7 @@
 module WellFormedness where
 
 import AST
+import Type
 
 import Control.Monad.RWS.Strict
 import Data.Function ((&))
@@ -16,12 +17,19 @@ data WFError = UnboundVar String
              | UnexpectedArrayLiteral Expr
              | BadAddrOf Expr
              | NegativeIndex Expr
+             | VoidVar String
+             | VoidReturn String
+             | MissingReturn String
+             | UnreachableStatement Statement
              | InternalError String
-             deriving(Eq, Ord, Show)
+             deriving(Eq, Show)
 
 type Checker a = (RWS Env [WFError] () a)
 
 -- utility
+
+nothing :: Monad m => m ()
+nothing = nothing
 
 varBound :: String -> Checker Bool
 varBound x = asks (x `elem`)
@@ -39,12 +47,12 @@ throwError = tell . (:[])
 
 checkExpr :: Expr -> Checker ()
 checkExpr = \case
-    EInt{} -> return ()
+    EInt{} -> nothing
     EVar x -> do
         b <- varBound x
-        if b then return () else throwError (UnboundVar x)
+        if b then nothing else throwError (UnboundVar x)
     EUnop AddrOf e -> (if isLHS e
-                       then return ()
+                       then nothing
                        else throwError (BadAddrOf e))
                       >> checkExpr e
     EUnop Deref e -> checkExpr e
@@ -55,39 +63,41 @@ checkExpr = \case
     e@(EGetIndex arr (EUnop Neg (EInt n))) | n > 0 -> checkExpr arr >> throwError (NegativeIndex e)
     EGetIndex arr idx -> checkExpr arr >> checkExpr idx
     e@(EArrayLiteral es) -> throwError (UnexpectedArrayLiteral e) >> mapM_ checkExpr es
+    EApp f args -> mapM_ checkExpr (f:args)
 
 checkLHS :: LHS -> Checker ()
 checkLHS = checkExpr . exprOfLhs
 
-checkDups :: [Statement] -> Checker ()
-checkDups b = mapM_ throwError errs
+checkDups :: [String] -> Checker ()
+checkDups vars = mapM_ throwError errs
     where
-        -- | only checks this level (doesn't go into ifs)
-        getNewVars = \case
-            Assign{} -> []
-            Return{} -> []
-            Decl _ x -> [x]
-            Def _ x _ -> [x]
-            If{} -> [] -- don't go deep
-            While{} -> []
-            For{} -> []
-        newVars = reverse $ b >>= getNewVars
-        errs = reverse $ go newVars
+        errs = reverse $ go vars
         go [] = []
         go (x:xs)
             | x `elem` xs = DupVar x:go xs
             | otherwise = go xs
 
+-- | only checks one level (doesn't go into ifs)
+getDeclaredVars :: [Statement] -> [String]
+getDeclaredVars b = reverse $ b >>= \case
+    Assign{} -> []
+    Return{} -> []
+    Decl _ x -> [x]
+    Def _ x _ -> [x]
+    If{} -> [] -- don't go deep
+    While{} -> []
+    For{} -> []
 
-
--- TODO check all paths return
--- TODO unreachable statements (need paramorphism)
 checkBlock :: [Statement] -> Checker ()
-checkBlock b = checkDups b >> foldr go (return ()) b
+checkBlock b = checkDups (getDeclaredVars b) >> go b
     where
-        go stmt mRest = case stmt of
+        go [] = nothing
+        go (stmt:rest) = let mRest = go rest in case stmt of
             Assign lhs rhs -> checkLHS lhs >> checkExpr rhs >> mRest
-            Return e -> checkExpr e >> mRest
+            Return e ->  checkExpr e >> checkUnreachable >> mRest
+                where checkUnreachable = case rest of
+                        [] -> nothing
+                        next:_ -> throwError (UnreachableStatement next)
             Decl _ x -> addVar x mRest
             -- need this special case because array literals are not well formed
             Def _ x (EArrayLiteral es) -> mapM_ checkExpr es >> addVar x mRest
@@ -95,14 +105,76 @@ checkBlock b = checkDups b >> foldr go (return ()) b
             If cnd thn mEls -> do
                 checkExpr cnd
                 checkBlock thn
-                maybe (return ()) checkBlock mEls
+                maybe nothing checkBlock mEls
                 mRest
             While cnd body -> checkExpr cnd >> checkBlock body >> mRest
             For setup cnd update body ->
                 checkBlock (whileOfFor setup cnd update body) >> mRest -- var not in scope for rest!
 
+checkSignature :: Type -> String -> [(Type, String)] -> Checker ()
+checkSignature _ _ targs = checkDups (snd <$> targs) >> checkVoids
+    where checkVoids = mapM_ (uncurry go) targs
+          go TVoid x = throwError (VoidVar x)
+          go _ _ = nothing
+
+-- | does the block of statements definitely return?
+doesReturn :: [Statement] -> Bool
+doesReturn = any go
+    where go = \case
+            Return{} -> True
+            If _ _ Nothing -> False
+            If _ thn (Just els) -> doesReturn thn && doesReturn els
+            -- setup may be return
+            For setup cnd update body -> doesReturn (whileOfFor setup cnd update body)
+            While{} -> False
+            Assign{} -> False
+            Decl{} -> False
+            Def{} -> False
+
+-- | is it possible for the block of statements to return?
+mayReturn :: [Statement] -> Bool
+mayReturn = any go
+    where go = \case
+            Return{} -> True
+            If _ thn mEls -> mayReturn thn || maybe False mayReturn mEls
+            For setup cnd update body -> mayReturn (whileOfFor setup cnd update body)
+            While _ body -> mayReturn body
+            Assign{} -> False
+            Decl{} -> False
+            Def{} -> False
+
+checkTopDecls :: [TopDecl] -> Checker ()
+checkTopDecls decls = checkDups (getDeclVars decls) >> checkDups (getDefVars decls) >> foldr go nothing decls
+    where
+        -- this separation is necessary bc you can declare, then define
+        getDeclVars [] = []
+        getDeclVars (FunDef{}:ds) = getDeclVars ds
+        getDeclVars (FunDecl _ f _:ds) = f:getDeclVars ds
+        getDefVars [] = []
+        getDefVars (FunDecl{}:ds) = getDeclVars ds
+        getDefVars (FunDef _ f _ _:ds) = f:getDeclVars ds
+        go decl mRest = case decl of
+            FunDecl _ f targs -> mapM_ go' targs >> addVar f mRest
+                where go' = \case
+                        (TVoid, x) -> throwError (VoidVar x)
+                        (TInt{},_) -> nothing
+                        (TRef{},_) -> nothing
+                        (TArray{},_) -> nothing
+                        (TFun{},_) -> nothing
+            FunDef ret f targs body -> do
+                go (FunDecl ret f targs) nothing
+                case ret of
+                    TVoid | mayReturn body -> throwError (VoidReturn f)
+                          | otherwise -> nothing
+                    _ | doesReturn body -> nothing
+                      | otherwise -> throwError (MissingReturn f)
+                -- f in scope for recursion!
+                -- f last to reflect the precedence of args over the function for recursion
+                addVars (fmap snd targs++[f]) $ checkBlock body
+                addVar f mRest
+
 checkProgram :: Program -> Checker ()
-checkProgram (Program block) = checkBlock block
+checkProgram (Program decls) = checkTopDecls decls
 
 -- running the monad
 
